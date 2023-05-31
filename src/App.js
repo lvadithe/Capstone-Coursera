5a5aa5d14630519a767c2cc264a1591a94193ec4
vadith gomez <legrone@MacBook-Pro.local>
2023-05-31 14:44:37 -0500

    capstone-front-end

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import Hightlights from "./components/Hightlights";
+import BookingForm from "./components/BookingForm";
 
 function App() {
+  const [bookingFormOpen, setBookingFormOpen] = useState(false);
+
+  function openBookingFormHandler() {
+    setBookingFormOpen(!bookingFormOpen);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {bookingFormOpen && (
+        <BookingForm onCloseLayout={openBookingFormHandler} />
+      )}
+      <Header onOpenLayout={openBookingFormHandler}></Header>
+      <Hero onOpenLayout={openBookingFormHandler}></Hero>
+      <Hightlights></Hightlights>
+      <footer style={{ height: "5rem" }}></footer>
+    </>
   );
 }
 
